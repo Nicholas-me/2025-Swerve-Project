2c4141c3e41fcab198f10717884637c578d0a9ce
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-03-03 22:13:05 -0500

    Cleaned up subsystems, imports in RobotContainer, constants, etc

--- src/main/java/frc/robot/commands/DriveForwardCommand.java
+++ src/main/java/frc/robot/commands/DriveForwardCommand.java
@@ -1,10 +1,7 @@
 package frc.robot.commands;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
