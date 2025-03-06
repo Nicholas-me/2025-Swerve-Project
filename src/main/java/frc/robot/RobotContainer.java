1239d165fa41cd196f674d9557adca3c56960fd1
Nicholas-me <nicholasjames096@gmail.com>
2025-03-06 17:00:19 -0500

    Tested Commands
    The commands worked, however, we need to have controls for the elevator itself. Could you help with it?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,13 +14,22 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+<<<<<<< Updated upstream
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Configs.CoralSubsystem;
+=======
+import frc.robot.Constants.OIConstants;
+>>>>>>> Stashed changes
 import frc.robot.commands.DriveForwardCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.AlgaeSubsystem;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.CoralSubsystem.Setpoint;
+<<<<<<< Updated upstream
+=======
+import edu.wpi.first.wpilibj.event.EventLoop;
+import edu.wpi.first.wpilibj2.command.Command;
+>>>>>>> Stashed changes
 
 public class RobotContainer {
 
@@ -121,6 +130,7 @@ public class RobotContainer {
                 .whileTrue(m_algaeSubsystem.runIntakeCommand());
         
 
+<<<<<<< Updated upstream
         // Trigger -> Run ball intake in reverse, set to stow when idle
          m_operatorController
                 .leftTrigger(/*OIConstants.kTriggerButtonThreshold*/)
@@ -130,6 +140,66 @@ public class RobotContainer {
         // Brake command if you want it
         // m_driverController.b().whileTrue(drivetrain.applyRequest(() -> brake));
 
+=======
+        // Set the default commands for a algae
+        m_algaeSubsystem.setDefaultCommand(m_algaeSubsystem.idleCommand());
+    }
+    
+    private void configureBindings() {
+        // Note that X is defined as forward according to WPILib convention,
+        // and Y is defined as to the left according to WPILib convention.
+        drivetrain.setDefaultCommand(
+                // Drivetrain will execute this command periodically
+                drivetrain
+                // Drive forward with negative Y (forward)
+                        .applyRequest(() -> drive.withVelocityX(-m_driverController.getLeftY() * MaxSpeed * SlowMoDrive) 
+                        // Drive left with negative X (left)
+                                .withVelocityY(-m_driverController.getLeftX() * MaxSpeed * SlowMoDrive) 
+                                // Drive counterclockwise with negative X (left)
+                                .withRotationalRate(-m_driverController.getRightX() * MaxAngularRate * SlowMoTurn) 
+                        ));
+    
+        /*
+         * If your robot controls don't seem to be going the right direction,
+         * rotate so the "front" of the robot faces away from you and is square
+         * Hit this start button to reset and recenter
+         */
+        m_driverController.start().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+    
+        // Left Bumper -> Run tube intake
+        m_operatorController.leftBumper().whileTrue(m_coralSubSystem.runIntakeCommand());
+
+        // Right Bumper -> Run tube intake in reverse
+        m_operatorController.rightBumper().whileTrue(m_coralSubSystem.reverseIntakeCommand());
+
+        // B Button -> Elevator/Arm to human player position, set ball intake to stow
+        // when idle
+        m_operatorController
+            .b()
+            .onTrue(
+                m_coralSubSystem
+                    .setSetpointCommand(Setpoint.kFeederStation)
+                    .alongWith(m_algaeSubsystem.stowCommand()));
+
+        // A Button -> Elevator/Arm to level 2 position
+        m_operatorController.a().onTrue(m_coralSubSystem.setSetpointCommand(Setpoint.kLevel2));
+
+        // X Button -> Elevator/Arm to level 3 position
+        m_operatorController.x().onTrue(m_coralSubSystem.setSetpointCommand(Setpoint.kLevel3));
+
+        // Y Button -> Elevator/Arm to level 4 position
+        m_operatorController.y().onTrue(m_coralSubSystem.setSetpointCommand(Setpoint.kLevel4));
+
+        // Right Trigger -> Run ball intake, set to leave out when idle
+        m_operatorController
+            .rightTrigger(OIConstants.kTriggerButtonThreshold)
+            .whileTrue(m_algaeSubsystem.runIntakeCommand());
+
+        // Left Trigger -> Run ball intake in reverse, set to stow when idle
+        m_operatorController
+            .leftTrigger(OIConstants.kTriggerButtonThreshold)
+            .whileTrue(m_algaeSubsystem.reverseIntakeCommand());
+>>>>>>> Stashed changes
 
     } // end of configureBindings
 
