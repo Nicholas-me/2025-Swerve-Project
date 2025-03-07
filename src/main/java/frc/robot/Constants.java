8653a95e10f7485368ed00beccd64d1db5939eae
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-03-06 21:38:33 -0500

    Remove commented-out constants and improve organization in Constants class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -65,69 +65,7 @@ public final class Constants {
       public static final double kHold = 0.25;
     }
   }
-  /*
-   * // SPARK MAX CAN IDs
-   * public static final int kFrontLeftDrivingCanId = 6;
-   * public static final int kRearLeftDrivingCanId = 12;
-   * public static final int kFrontRightDrivingCanId = 8;
-   * public static final int kRearRightDrivingCanId = 10;
-   * 
-   * public static final int kFrontLeftTurningCanId = 5;
-   * public static final int kRearLeftTurningCanId = 11;
-   * public static final int kFrontRightTurningCanId = 7;
-   * public static final int kRearRightTurningCanId = 9;
-   * 
-   * public static final boolean kGyroReversed = false;
-   * }
-   * 
-   * public static final class ModuleConstants {
-   * // The MAXSwerve module can be configured with one of three pinion gears:
-   * 12T,
-   * // 13T, or 14T. This changes the drive speed of the module (a pinion gear
-   * with
-   * // more teeth will result in a robot that drives faster).
-   * public static final int kDrivingMotorPinionTeeth = 14;
-   * 
-   * // Calculations required for driving motor conversion factors and feed
-   * forward
-   * public static final double kDrivingMotorFreeSpeedRps =
-   * NeoMotorConstants.kFreeSpeedRpm / 60;
-   * public static final double kWheelDiameterMeters = 0.0762;
-   * public static final double kWheelCircumferenceMeters = kWheelDiameterMeters *
-   * Math.PI;
-   * // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear,
-   * 15
-   * // teeth on the bevel pinion
-   * public static final double kDrivingMotorReduction =
-   * (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
-   * public static final double kDriveWheelFreeSpeedRps =
-   * (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters) /
-   * kDrivingMotorReduction;
-   * }
-   * 
-   * public static final class OIConstants {
-   * 
-   * }
-   * 
-   * public static final class AutoConstants {
-   * public static final double kMaxSpeedMetersPerSecond = 3;
-   * public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-   * public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-   * public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-   * 
-   * public static final double kPXController = 1;
-   * public static final double kPYController = 1;
-   * public static final double kPThetaController = 1;
-   * 
-   * // Constraint for the motion profiled robot angle controller
-   * public static final TrapezoidProfile.Constraints kThetaControllerConstraints
-   * =
-   * new TrapezoidProfile.Constraints(
-   * kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-   * }
-   * 
-   * }
-   */
+
   public static final double kNEOFreeSpeedRpm = 5676;
  
   public static final int kDriverControllerPort = 0;
